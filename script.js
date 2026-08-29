4d5f45349dc7afb73db6292ded617a3ca836324d
riverojsx <dvxjsx@gmail.com>
2026-08-29 05:30:02 -0500

    Add MemoryBook V2 script

---
+++ script.js
@@ -0,0 +1,4 @@
+// Base para MemoryBook V2
+// Aquí se generarán dinámicamente las hojas 1|2, 3|4, etc.
+// Esta plantilla está lista para ampliarse.
+document.getElementById('book').textContent='MemoryBook V2 listo para desarrollar';
